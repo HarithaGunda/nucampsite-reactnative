65e1b8209624770f2c51fece50340963a722f197
Haritha Gunda <gunda.haritha@gmail.com>
2020-02-19 11:33:03 -0500

    Swipeout and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,7 +4,10 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
+const mapDispatchToProps = {    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))};
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
@@ -21,13 +24,22 @@ class Favorites extends Component {
     render() {
         const { navigate } = this.props.navigation;
         const renderFavoriteItem = ({item}) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                        onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+                    />
+                </Swipeout>
             );
         };
 
@@ -53,4 +65,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps,mapDispatchToProps)(Favorites);
